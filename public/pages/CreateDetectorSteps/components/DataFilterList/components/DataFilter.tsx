546d810a24c6392b94508ff59c774c464070afbf
Tyler Ohlsen <ohltyler@amazon.com>
2021-03-02 18:51:39 -0800

    Add header and footer for filter popover

--- public/pages/CreateDetectorSteps/components/DataFilterList/components/DataFilter.tsx
+++ public/pages/CreateDetectorSteps/components/DataFilterList/components/DataFilter.tsx
@@ -20,11 +20,17 @@ import {
   EuiBadge,
   EuiButtonEmpty,
   EuiPopover,
+  EuiPopoverTitle,
   EuiText,
+  EuiSwitch,
+  EuiFieldText,
+  EuiSpacer,
 } from '@elastic/eui';
 import React, { useState } from 'react';
 import { UIFilter } from '../../../../../models/interfaces';
 import { SimpleFilter } from '../components/SimpleFilter';
+import { CustomFilter } from '../components/CustomFilter';
+import { FormattedFormRow } from '../../FormattedFormRow/FormattedFormRow';
 
 interface DataFilterProps {
   filter: UIFilter;
@@ -44,6 +50,8 @@ export const DataFilter = (props: DataFilterProps) => {
   const [isSimple, setIsSimple] = useState<boolean>(true);
   const setToSimple = () => setIsSimple(true);
   const setToCustom = () => setIsSimple(false);
+
+  const [isCustomLabel, setIsCustomLabel] = useState<boolean>(false);
 
   const badge = (
     <EuiBadge
@@ -73,55 +81,92 @@ export const DataFilter = (props: DataFilterProps) => {
         button={badge}
         isOpen={isPopoverOpen}
         closePopover={closePopover}
+        anchorPosition="downCenter"
       >
-        <EuiFlexGroup direction="row">
-          <EuiFlexItem>
-            <EuiText>{`Data filter with label ${getFilterLabel(
-              props.filter
-            )}`}</EuiText>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiButtonEmpty
-              onClick={() => {
-                isSimple ? setToCustom() : setToSimple();
-              }}
-            >
-              {isSimple ? 'Use query DSL' : 'Use visual editor'}
-            </EuiButtonEmpty>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-        {isSimple ? <SimpleFilter filter={props.filter} /> : null}
-        <EuiFlexGroup
-          alignItems="center"
-          justifyContent="flexEnd"
-          gutterSize="s"
-          style={{ marginRight: '12px' }}
-        >
-          <EuiFlexItem grow={false}>
-            <EuiButtonEmpty
-              data-test-subj={`cancelFilter${props.index}Button`}
-              onClick={() => {
-                console.log('canceling');
-                closePopover();
-              }}
-            >
-              Cancel
-            </EuiButtonEmpty>
-          </EuiFlexItem>
-          <EuiFlexItem grow={false}>
-            <EuiButton
-              //type="submit"
-              fill={true}
-              data-test-subj={`saveFilter${props.index}Button`}
-              //isLoading={formikProps.isSubmitting}
-              onClick={() => {
-                console.log('saving');
-                closePopover();
-              }}
-            >
-              Save
-            </EuiButton>
-          </EuiFlexItem>
+        <EuiPopoverTitle>
+          <EuiFlexGroup
+            direction="row"
+            alignItems="center"
+            style={{ margin: '-18px' }}
+          >
+            <EuiFlexItem>
+              <EuiText>
+                <p style={{ textTransform: 'none' }}>
+                  <b>Data filter</b>
+                </p>
+              </EuiText>
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <EuiButtonEmpty
+                onClick={() => {
+                  isSimple ? setToCustom() : setToSimple();
+                }}
+              >
+                {isSimple ? 'Use query DSL' : 'Use visual editor'}
+              </EuiButtonEmpty>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiPopoverTitle>
+        {isSimple ? (
+          <SimpleFilter filter={props.filter} />
+        ) : (
+          <CustomFilter filter={props.filter} />
+        )}
+        <EuiSpacer />
+        <EuiFlexGroup direction="column" style={{ margin: '0px' }}>
+          <EuiFlexGroup direction="column">
+            <EuiFlexItem>
+              <EuiSwitch
+                label={<EuiText>Create custom label?</EuiText>}
+                checked={isCustomLabel}
+                onChange={() => setIsCustomLabel(!isCustomLabel)}
+              />
+            </EuiFlexItem>
+            {isCustomLabel ? (
+              <EuiFlexItem>
+                <FormattedFormRow title="Custom label">
+                  <EuiFieldText
+                    name="customLabel"
+                    id="customLabel"
+                    placeholder="Enter a value"
+                  />
+                </FormattedFormRow>
+              </EuiFlexItem>
+            ) : null}
+          </EuiFlexGroup>
+          <EuiSpacer />
+          <EuiFlexGroup
+            alignItems="center"
+            justifyContent="flexEnd"
+            gutterSize="s"
+            //style={{ marginRight: '12px' }}
+          >
+            <EuiFlexItem grow={false}>
+              <EuiButtonEmpty
+                data-test-subj={`cancelFilter${props.index}Button`}
+                onClick={() => {
+                  console.log('canceling');
+                  closePopover();
+                }}
+              >
+                Cancel
+              </EuiButtonEmpty>
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <EuiButton
+                //type="submit"
+                fill={true}
+                data-test-subj={`saveFilter${props.index}Button`}
+                //isLoading={formikProps.isSubmitting}
+                onClick={() => {
+                  console.log('saving');
+                  closePopover();
+                }}
+              >
+                Save
+              </EuiButton>
+            </EuiFlexItem>
+          </EuiFlexGroup>
         </EuiFlexGroup>
       </EuiPopover>
     </EuiFlexItem>
